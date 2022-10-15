bd095961d9e85001e280644a0e14109b03046ef5
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-10-15 06:04:37 +0100

    Added redirecting and wildcard for 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {
@@ -25,7 +26,9 @@ const appRoutes: Routes = [
       { path: ':id', component: ServerComponent },
     ]
   },
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  {path: 'not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/not-found'} // needs to be last route as routes parsed from top to bottom
 ];
 
 @NgModule({
@@ -36,7 +39,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
